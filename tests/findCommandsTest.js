f65d5f7c2e99815249467e6a9d8e2bf7db1a253b
Yehiyam Livneh <yehiyam@gmail.com>
2020-08-13 18:55:22 +0300

    initial

---
+++ tests/findCommandsTest.js
@@ -0,0 +1,86 @@
+const { expect } = require('chai');
+const fs = require('fs');
+const { parseCommands, parseDescription,parseArgs, findCommandsRecursive } = require('../lib/findCommands');
+const createCommand = require('../lib/command');
+const { exec } = require('child_process');
+describe('find tests', () => {
+
+    describe('regex tests', () => {
+        it('empty string', () => {
+            const helpString = '';
+            const res = parseCommands('hkubectl', helpString)
+            expect(res).to.have.lengthOf(0)
+        });
+        it('one line', () => {
+            const helpString = 'hkubectl [command]\nhkubectl exec <command>';
+            const res = parseCommands('hkubectl', helpString)
+            expect(res).to.have.lengthOf(1);
+            expect(res[0]).to.eql('exec');
+        });
+        it('two line', () => {
+            const helpString = 'hkubectl [command]\nhkubectl exec <command>\nhkubectl algorithm <command>';
+            const res = parseCommands('hkubectl', helpString)
+            expect(res).to.have.lengthOf(2);
+            expect(res[0]).to.eql('exec');
+            expect(res[1]).to.eql('algorithm');
+        });
+        it('multi line', () => {
+            const helpString = fs.readFileSync(__dirname + '/mocks/text1', 'utf-8');
+            const res = parseCommands('hkubectl', helpString)
+            expect(res).to.have.lengthOf(7);
+            expect(res[0]).to.eql('exec');
+            expect(res[1]).to.eql('algorithm');
+        });
+        it('no command', () => {
+            const helpString = fs.readFileSync(__dirname + '/mocks/textNoCommand', 'utf-8');
+            const res = parseCommands('hkubectl', helpString)
+            expect(res).to.have.lengthOf(0);
+        });
+        it('no match', () => {
+            const helpString = 'foo exec <command>';
+            const res = parseCommands('hkubectl', helpString)
+            expect(res).to.have.lengthOf(0);
+        });
+    });
+    describe('description', () => {
+        it('parse empty', () => {
+            const helpString = fs.readFileSync(__dirname + '/mocks/text1', 'utf-8');
+            const res = parseDescription('hkubectl', helpString);
+            expect(res).to.eql('')
+        });
+        it('parse description', () => {
+            const helpString = fs.readFileSync(__dirname + '/mocks/textCommand', 'utf-8');
+            const res = parseDescription('hkubectl', helpString);
+            expect(res).to.eql('Execution pipelines as raw or stored')
+        });
+        it('parse description no command', () => {
+            const helpString = fs.readFileSync(__dirname + '/mocks/textNoCommand', 'utf-8');
+            const res = parseDescription('hkubectl', helpString);
+            expect(res).to.eql('Lists all registered algorithms')
+        });
+    });
+    describe('args', () => {
+        it('parse one arg', () => {
+            const helpString = fs.readFileSync(__dirname + '/mocks/textNoCommandArg', 'utf-8');
+            const res = parseArgs('hkubectl', helpString,'exec get');
+            expect(res).to.have.lengthOf(1)
+            expect(res[0].name).to.eql('name')
+            expect(res[0].optional).to.eql(false)
+        });
+        it('parse no arg', () => {
+            const helpString = fs.readFileSync(__dirname + '/mocks/textNoCommand', 'utf-8');
+            const res = parseArgs('hkubectl', helpString,'exec get');
+            expect(res).to.have.lengthOf(0)
+        });
+    });
+    describe('recursive tests', () => {
+        // it('simple', async () => {
+        //     const res = await findCommandsRecursive('hkubectl', `${__dirname}/mocks/hkubectl`)
+        //     expect(res).to.not.be.empty
+        // }).timeout(15000);
+        it('class', async () => {
+            const command = await createCommand('root', 'hkubectl', `${__dirname}/mocks/hkubectl`)
+            expect(command).to.exist
+        }).timeout(15000);;
+    });
+});
